f9f53b79e14972f34793b4289210eb562295ff27
cornernote <cornernote@gmail.com>
2015-07-15 17:39:35 +0930

    added pgsql and sqlite to tests

--- tests/codeception/_app/config/db.php
+++ tests/codeception/_app/config/db.php
@@ -1,5 +1,35 @@
 <?php
 
+if (YII_ENV == 'heroku') {
+    $url = parse_url(getenv('DATABASE_URL'));
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'pgsql:host=' . $url['host'] . ';dbname=' . substr($url['path'], 1),
+        'username' => $url['user'],
+        'password' => $url['pass'],
+        'charset' => 'utf8',
+    ];
+}
+
+if (getenv('DB') == 'pgsql') {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'pgsql:host=localhost;dbname=dashboard_test',
+        'username' => 'travis',
+        'password' => '',
+        'charset' => 'utf8',
+    ];
+}
+
+if (getenv('DB') == 'sqlite') {
+    return [
+        'class' => 'yii\db\Connection',
+        //'dsn' => 'sqlite::memory:', // not persistent ?
+        'dsn' => 'sqlite:' . dirname(__DIR__) . '/runtime/sqlite_test.db',
+        'charset' => 'utf8',
+    ];
+}
+
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=dashboard_test',
